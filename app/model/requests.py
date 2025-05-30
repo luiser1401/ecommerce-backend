b989f9f00ebbd29fe9ba9ef22fbd5544e662f9b7
Luis <luislopezsdbustamante@gmail.com>
2025-05-30 11:15:20 +0200

    add request

---
+++ app/model/requests.py
@@ -0,0 +1,31 @@
+import datetime
+import uuid
+
+from pydantic import BaseModel, ConfigDict, UUID4, Field
+
+
+class EcomRequest(BaseModel):
+    model_config = ConfigDict(
+        arbitrary_types_allowed=True,
+        populate_by_name=True
+    )
+
+    request_id: UUID4 = Field(
+        default_factory=uuid.uuid4
+    )
+
+    name: str
+    description: str
+    price: float
+    discount_price: float
+    stock_quantity: int
+    sku: str
+    image_ur: str
+    category: str
+    brand: str
+    weight: str
+    dimensions: str  # format: LxWxH in cm
+    is_active: bool
+    is_featured: bool
+    created_at: datetime.datetime = Field(default_factory=datetime.datetime.utcnow)
+    updated_at: datetime.datetime = Field(default_factory=datetime.datetime.utcnow)
