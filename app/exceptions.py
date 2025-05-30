e569e09258306529ee40f5c2f7a50e1c3db965a4
Luis <luislopezsdbustamante@gmail.com>
2025-05-30 11:26:30 +0200

    update exceptuons

--- app/exceptions.py
+++ app/exceptions.py
@@ -2,7 +2,4 @@
 
 class EcommerceBackendException(Exception):
     """Base exception for all ecommerce backend errors."""
-    
-    def __init__(self, message: str = "An error occurred in the ecommerce backend"):
-        self.message = message
-        super().__init__(self.message)
+    pass
